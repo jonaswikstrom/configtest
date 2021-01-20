f69c9a5b7a9d486810d3ea8a33d02ef2a837bf8e
Jonas Wikström <jonas@jonaswikstrom.se>
2021-01-20 12:16:31 +0100

    First draft

---
+++ ConfigurationManagementTest/Controllers/ConfigurationController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationManagementTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly IOptions<Credentials> credentials;
+
+        public ConfigurationController(IConfiguration configuration, IOptions<Credentials> credentials)
+        {
+            this.configuration = configuration;
+            this.credentials = credentials;
+        }
+
+        [HttpPost, Route("{key}/{value}")]
+        public IActionResult Post(string key, string value)
+        {
+            Environment.SetEnvironmentVariable(key, value);
+            return Ok($"{key}:{value}");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"{credentials.Value.UserName} : {credentials.Value.Password}");
+        }
+
+        [HttpGet, Route("{key}")]
+        public IActionResult Get(string key)
+        {
+            var value = configuration.GetValue<string>(key);
+            return Ok(value);
+        }
+    }
+}
